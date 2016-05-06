8c5870b4c6c1ff674946815935cd64af74fcad57
Vincent van Diermen <vincent@tygron.com>
2016-05-06 11:31:38 +0200

    Added CG5 Client LVL2 - WIP

---
+++ Multiplayer-Cg5-Client-LVL2-160506-VVD-0.1.sikuli/Multiplayer-Cg5-Client-LVL2-160506-VVD-0.1.py
@@ -0,0 +1,46 @@
+import logging; reload(logging)
+FORMAT=("%(asctime) -8s %(message) s")
+logging.basicConfig(format=FORMAT, filename="TestLog.txt", level=logging.DEBUG)
+
+logging.info("[info] Play Level 2...")
+
+if Settings.isLinux() or Settings.isWindows():
+
+    ################
+    # Municipality #
+    ################
+
+    ##############
+    # Waterboard #
+    ##############
+
+    #######
+    # SSH #
+    #######
+
+    ###################
+    # UNI Real Estate #
+    ###################
+
+elif Settings.isMac():
+
+    ################
+    # Municipality #
+    ################
+
+    ##############
+    # Waterboard #
+    ##############
+
+    #######
+    # SSH #
+    #######
+
+    ###################
+    # UNI Real Estate #
+    ###################
+
+else:
+    print"[error] OS error!"
+    logging.error("[error] OS error!")
+    exit(1)
