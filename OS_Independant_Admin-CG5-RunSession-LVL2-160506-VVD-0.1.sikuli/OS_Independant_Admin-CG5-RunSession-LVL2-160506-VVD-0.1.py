36220de6966fa6d09d99533ff7256160bc8874a2
Vincent van Diermen <vincent@tygron.com>
2016-05-06 14:17:20 +0200

    Updated admin lvl2

--- OS_Independant_Admin-CG5-RunSession-LVL2-160506-VVD-0.1.sikuli/OS_Independant_Admin-CG5-RunSession-LVL2-160506-VVD-0.1.py
+++ OS_Independant_Admin-CG5-RunSession-LVL2-160506-VVD-0.1.sikuli/OS_Independant_Admin-CG5-RunSession-LVL2-160506-VVD-0.1.py
@@ -4,3 +4,10 @@ logging.basicConfig(format=FORMAT, filename="testLog.txt", level=logging.DEBUG)
 
 logging.info("[info] Admin runs session LVL2...")
 
+if Settings.isLinux() or Settings.isWindows():
+
+elif Settings.isMac():
+    print"WIP - MAC not scripted yet!"
+
+else:
+    print"OS error!"
