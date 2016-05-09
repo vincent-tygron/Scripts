4cf42b0621201d1892c36323bb5d852935c25fb8
Vincent van Diermen <vincent@tygron.com>
2016-05-09 15:54:23 +0200

    admin fixes

--- OS_Independant_Admin-StartSession-160422-VVD-0.1.sikuli/OS_Independant_Admin-StartSession-160422-VVD-0.1.py
+++ OS_Independant_Admin-StartSession-160422-VVD-0.1.sikuli/OS_Independant_Admin-StartSession-160422-VVD-0.1.py
@@ -5,10 +5,20 @@ logging.basicConfig(format=FORMAT, filename="testLog.txt", level=logging.DEBUG)
 logging.info("[info] Admin starts session...")
 
 if Settings.isLinux() or Settings.isWindows():
-    wait("Admin-MultiPlayer-SettingsBtn-160422-VVD-0.1.png")
-    click()
-    wait("Admin-MultiPlayer-Settings-StartSessionBtn-160422-VVD-0.2.png")
-    click()
+#########################################
+# Check if all clients are connected... #
+#########################################
+    if exists("Admin-MultiPlayer-FourClientsConnected-160422-VVD-0.1.png", 30):#Add 30 seconds if needed
+        print"[success] Clients were successfully invited and connected!"
+        logging.info("[success] Clients were successfully invited and connected!")
+    else:
+        print"[error] Clients were not connected (in time)!"
+        logging.error("[error] Clients were not connected (in time)!")
+        exit(1)
+        
+    click("Admin-MultiPlayer-SettingsBtn-160422-VVD-0.1.png")
+    click("Admin-MultiPlayer-Settings-StartSessionBtn-160422-VVD-0.2.png")
+    
     if exists("Admin-MultiPlayer-Settings-SessionsAreRunning-160422-VVD-0.1.png", 30):
         print"[success] Session has started!"
         logging.info("[success] Session has started!")
@@ -16,3 +26,7 @@ if Settings.isLinux() or Settings.isWindows():
         print"[error] Session did not start in time and/ or as expected!"
         logging.error("[error] Session did not start in time and/ or as expected!")
         exit(1)
+
+else:
+    #OSX - WIP
+    exit(1)
--- OS_Independant_AdminAssigningClients-160422-VVD-0.2.sikuli/OS_Independant_AdminAssigningClients-160422-VVD-0.2.py
+++ OS_Independant_AdminAssigningClients-160422-VVD-0.2.sikuli/OS_Independant_AdminAssigningClients-160422-VVD-0.2.py
@@ -9,21 +9,9 @@ if Settings.isLinux() or Settings.isWindows():
     for x in range(0, 10):
         while exists("MainMenu-MultiPlayer-ClientBtn-160426-VVD-0.1.png", x):
             click()
-            click("MainMenu-MultiPlayer-SettingsBtn-160426-VVD-0.1.png")
+            click("Admin-CG5-LevelBtn-160428-VVD-0.1.png")
+            click("MainMenu-MultiPlayer-SettingsBtn-160509-VVD-0.1.png")
             click("MainMenu-MultiPlayer-ServerBtn-160426-VVD-0.1.png")
-            #waitVanish("MainMenu-MultiPlayer-ClientBtn-160426-VVD-0.1.png")
             break
-
-#########################################
-# Check if all clients are connected... #
-#########################################
-    if exists("Admin-MultiPlayer-FourClientsConnected-160422-VVD-0.1.png", 30):
-        print"[success] Clients were successfully invited and connected!"
-        logging.info("[success] Clients were successfully invited and connected!")
-    else:
-        print"[error] Clients were not connected (in time)!"
-        logging.error("[error] Clients were not connected (in time)!")
-        exit(1)
-
 else:
     exit(1) #OSX >>> Wip
