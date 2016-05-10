b40e2c385955d460bd00f39752d36cd891f709b3
Vincent van Diermen <vincent@tygron.com>
2016-05-10 13:32:25 +0200

    AcceptInvite updated
    Key.ENTER ipv click()

--- Multiplayer-Cg5-AcceptInvite-160422-VVD-0.2.sikuli/Multiplayer-Cg5-AcceptInvite-160422-VVD-0.2.py
+++ Multiplayer-Cg5-AcceptInvite-160422-VVD-0.2.sikuli/Multiplayer-Cg5-AcceptInvite-160422-VVD-0.2.py
@@ -6,7 +6,7 @@ logging.info("[info] Accept invite and load Cg5 session...")
 
 if Settings.isLinux() or Settings.isWindows():
     wait("Multiplayer-Client-InviteMessage-160422-VVD-0.1.png", FOREVER)
-    click("Multiplayer-Client-InviteMessage-YesBtn-160422-VVD-0.1.png")
+    type(Key.ENTER)
     if exists("Multiplayer-Client-Cg5-SplashScreen-160422-VVD-0.1.png", 60):
         print"[success] Client is connected to session!"
         logging.info("[success] Client is connected to session!")
@@ -17,7 +17,7 @@ if Settings.isLinux() or Settings.isWindows():
 
 if Settings.isMac():
     wait("Multiplayer-Client-MAC-InviteMessage-160425-VVD-0.1.png", FOREVER)
-    click("Multiplayer-Client-MAC-InviteMessage-YesBtn-160425-VVD-0.1.png")
+    type(Key.ENTER)
     if exists("Multiplayer-Client-MAC-Cg5-SplashScreen-160425-VVD-0.1.png", 60):
         print"[success] Client is connected to session!"
         logging.info("[success] Client is connected to session!")
