ed50c08114fc1c8b2486d4aa5592a0e2233e4c21
Vincent van Diermen <vincent@tygron.com>
2016-05-10 11:10:15 +0200

    Fix LVL2 Win 10 Muni

--- Multiplayer-Cg5-Client-LVL2-160506-VVD-0.1.sikuli/Multiplayer-Cg5-Client-LVL2-160506-VVD-0.1.py
+++ Multiplayer-Cg5-Client-LVL2-160506-VVD-0.1.sikuli/Multiplayer-Cg5-Client-LVL2-160506-VVD-0.1.py
@@ -6,13 +6,15 @@ logging.info("[info] Play Level 2...")
 
 if Settings.isLinux() or Settings.isWindows():
 
-    wait("Multiplayer-CG5-IntroPane-CntBtnl-160506-VVD-0.1.png", FOREVER)
+    if exists("Multiplayer-CG5-IntroPane-CntBtnl-160506-VVD-0.1.png"):
+        print"[info] Level 2 has started..."
+        logging.info("[info] Level 2 has started...")
 
     ################
     # Municipality #
     ################
 
-    if exists(Pattern("Multiplayer-CG5-Municipality-LVL2-IntroPanel-160506-VVD-0.1.png").similar(0.80)):
+    if exists(Pattern("Multiplayer-CG5-Municipality-LVL2-IntroPanel-160506-VVD-0.1.png").similar(0.80), 0):
         print"[info] Municipality switched to Level 2..." 
         logging.info("[info] Municipality switched to Level 2...")
         
@@ -24,7 +26,7 @@ if Settings.isLinux() or Settings.isWindows():
     ##############
     # Waterboard #
     ##############
-    elif exists("Multiplayer-CG5-Waterboard-LVL2-IntroPanel-160506-VVD-0.1.png"):
+    elif exists("Multiplayer-CG5-Waterboard-LVL2-IntroPanel-160506-VVD-0.1.png", 0):
         print"[info] Waterboard switched to Level 2..." 
         logging.info("[info] Waterboard switched to Level 2...")
         
@@ -36,7 +38,7 @@ if Settings.isLinux() or Settings.isWindows():
     #######
     # SSH #
     #######
-    elif exists("Multiplayer-CG5-SSH-LVL2-IntroPanel-160506-VVD-0.1.png"):
+    elif exists("Multiplayer-CG5-SSH-LVL2-IntroPanel-160506-VVD-0.1.png", 0):
         print"[info] SSH switched to Level 2..." 
         logging.info("[info] SSH switched to Level 2...")
         
@@ -60,14 +62,14 @@ if Settings.isLinux() or Settings.isWindows():
         
 elif Settings.isMac():
 
-    if exists("Multiplayer-CG5-MAC-IntroPane-CntBtnl-160506-VVD-0.1.png", FOREVER):
+    if exists("Multiplayer-CG5-MAC-IntroPane-CntBtnl-160506-VVD-0.1.png"):
         print"[info] Level 2 has started..."
         logging.info("[info] Level 2 has started...")
 
     ################
     # Municipality #
     ################
-    if exists(Pattern("Multiplayer-CG5-MAC-Municipality-LVL2-IntroPanel-160506-VVD-0.1.png").similar(0.90)):
+    if exists(Pattern("Multiplayer-CG5-MAC-Municipality-LVL2-IntroPanel-160506-VVD-0.1.png").similar(0.90), 0):
         print"[info] Municipality switched to Level 2..."
         logging.info("[info] Municipality switched to Level 2...")
 
@@ -78,7 +80,14 @@ elif Settings.isMac():
     ##############
     # Waterboard #
     ##############
-
+    elif exists("Multiplayer-CG5-MAC-Waterboard-LVL2-IntroPanel-160510-VVD-0.1.png", 0):
+        print"[info] Waterboard switched to Level 2..." 
+        logging.info("[info] Waterboard switched to Level 2...")
+        
+        loc = SCREEN.getCenter()
+        click(loc)
+        click(loc)
+        type(Key.SPACE)
     #######
     # SSH #
     #######
