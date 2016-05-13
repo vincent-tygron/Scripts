a457672903daad99f3d26a4ff5523b26c54e5ca2
Vincent van Diermen <vincent@tygron.com>
2016-05-13 14:03:09 +0200

    little Admin update
    Made search better for clients available to join

--- OS_Independant_AdminAssigningClients-160422-VVD-0.2.sikuli/OS_Independant_AdminAssigningClients-160422-VVD-0.2.py
+++ OS_Independant_AdminAssigningClients-160422-VVD-0.2.sikuli/OS_Independant_AdminAssigningClients-160422-VVD-0.2.py
@@ -7,7 +7,7 @@ logging.info("[info] Assigning clients to team...")
 if Settings.isLinux() or Settings.isWindows():
 
     for x in range(0, 10):
-        while exists("MainMenu-MultiPlayer-ClientBtn-160426-VVD-0.1.png", x):
+        while exists(Pattern("MainMenu-MultiPlayer-ClientBtn-160426-VVD-0.1.png").similar(0.79), x):
             click()
             click("Admin-CG5-LevelBtn-160428-VVD-0.1.png")
             click("MainMenu-MultiPlayer-SettingsBtn-160509-VVD-0.1.png")
